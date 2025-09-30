3c9bc3307b6e004533df04f4522847bfee4bfafa
uiiai <106063@stu.shps.tp.edu.tw>
2025-09-30 15:06:59 +0800

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i =0 ; i < 12; i++)
+            {
+                for (int j =0 ; j < 8 ; j++)
+                {
+                    textBox2.Text += "■";
+                }
+                textBox2.Text  +=Environment.NewLine;
+            }
+        }
+    }
+}
